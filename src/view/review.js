b260221549ab459bf7d0844586f45b447abb5312
li2199 <1269001954@qq.com>
2021-07-21 17:26:47 +0800

    1 file modified

--- src/view/review.js
+++ src/view/review.js
@@ -1,59 +1,73 @@
 import { Table, Space, Button } from 'antd';
-import React from 'react';
-const columns = [
-  {
-    title: '用户名',
-    dataIndex: 'uid',
-    key: 'uid',
-    render: text => <a>{text}</a>
-  },
-  {
-    title: '城市id',
-    dataIndex: 'cityid',
-    key: 'cityid'
-  },
-  {
-    title: '关联店铺',
-    dataIndex: 'sname',
-    key: 'sname'
-  },
-  {
-    title: '标题',
-    dataIndex: 'title',
-    key: 'title'
-  },
-  {
-    title: '内容',
-    dataIndex: 'details',
-    key: 'details'
-  },
-  {
-    title: '审核状态',
-    dataIndex: 'review',
-    key: 'review'
-  },
-  {
-    title: '操作',
-    key: 'action',
-    render: (text, record) => (
-      <Space size="middle">
-        <Button type="primary" onClick={()=>change(record.key)}>修改</Button>
-        <Button type="default" className="leave" onClick={()=>del(record.key)}>
-          删除
-        </Button>
-      </Space>
-    )
-  }
-];
-function change(e){
-  console.log(e)
-}
-function del(e){
-  console.log(e)
-}
+import React, { useState } from 'react';
+import axios from 'axios';
 export default props => {
-  console.log(props);
-  const title = props.location.state.title;
+  const columns = [
+    {
+      title: '用户名',
+      dataIndex: 'uid',
+      key: 'uid',
+      render: text => <a>{text}</a>
+    },
+    {
+      title: '城市id',
+      dataIndex: 'cityid',
+      key: 'cityid'
+    },
+    {
+      title: '关联店铺',
+      dataIndex: 'sname',
+      key: 'sname'
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      key: 'title'
+    },
+    {
+      title: '内容',
+      dataIndex: 'details',
+      key: 'details'
+    },
+    {
+      title: '审核状态',
+      dataIndex: 'review',
+      key: 'review'
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <Button type="primary" onClick={() => change(record)}>
+            审核通过
+          </Button>
+          <Button type="default" className="leave" onClick={() => del(record)}>
+            删除
+          </Button>
+        </Space>
+      )
+    }
+  ];
+
+  function change(e) {
+    axios
+      .post('http://localhost:9000/reviewtitle', {
+        index: e.key,
+        review: 'OK'
+      })
+      .then(res => setTitle(res.data));
+  }
+  function del(e) {
+    axios
+      .post('http://localhost:9000/reviewtitle', {
+        index: e.key,
+        review: 'del'
+      })
+      .then(res => setTitle(res.data));
+  }
+  const [title, setTitle] = useState(props.location.state.title);
+
   title.map((i, index) => {
     i.key = index;
   });
